f047192541818f32cac04729efcea3a087989958
Ilya Erlingas <Aligruit@gmail.com>
2020-06-02 14:30:59 +0300

    _

--- orm_migrations/school/views.py
+++ orm_migrations/school/views.py
@@ -6,9 +6,8 @@ from .models import Student
 
 def students_list(request):
     template = 'school/students_list.html'
-    objects_list = Student.objects.prefetch_related('teacher').all()
-    # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
     ordering = 'group'
+    objects_list = Student.objects.all()
     context = {'object_list': objects_list}
     return render(request, template, context)
