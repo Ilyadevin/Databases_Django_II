9fd348f7e7b0f3d8b3b17f4cfdeb6ee7b13abbec
Ilya Erlingas <Aligruit@gmail.com>
2020-06-04 00:45:24 +0300

    tegs added

--- m2m-relations/articles/models.py
+++ m2m-relations/articles/models.py
@@ -2,11 +2,10 @@ from django.db import models
 
 
 class Article(models.Model):
-
     title = models.CharField(max_length=256, verbose_name='Название')
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
-    image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    image = models.ImageField(null=True, blank=True, verbose_name='Изображение', )
 
     class Meta:
         verbose_name = 'Статья'
@@ -14,3 +13,11 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=25, default=1)
+    main = models.ManyToManyField(Article, related_name='tags')
+
+    def __str__(self):
+        return self.name
