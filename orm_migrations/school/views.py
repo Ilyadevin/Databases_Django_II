fa703304f47c029307234c2cc4e84e3e2faa51cd
Aligruit@gmail.com <Aligruit@gmail.com>
2020-06-07 19:41:43 +0300

    fix

--- orm_migrations/school/views.py
+++ orm_migrations/school/views.py
@@ -1,12 +1,11 @@
 from django.views.generic import ListView
 from django.shortcuts import render
 
-from .models import Student
+from .models import Student, Teacher
 
 
 def students_list(request):
     template = 'school/students_list.html'
-    ordering = 'group'
-    objects_list = Student.objects.all().order_by(ordering)
-    context = {'object_list': objects_list}
+    objects_list = Student.objects.all().order_by('group')
+    context = {'object_list': objects_list, }
     return render(request, template, context)
