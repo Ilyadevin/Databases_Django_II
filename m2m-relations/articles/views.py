ec271943b013d110c417a6717832455cc564f649
Ilya Erlingas <Aligruit@gmail.com>
2020-06-22 16:07:37 +0300

    updating some parts

--- m2m-relations/articles/views.py
+++ m2m-relations/articles/views.py
@@ -5,6 +5,7 @@ from .models import Article, PublishedArticles
 def articles_list(request):
     template = 'articles/news.html'
     ordering = '-published_at'
-    articles = PublishedArticles.objects.all().order_by(ordering)
-    context = {'object_list': PublishedArticles,}
+    articles = Article.objects.all()
+    tags = PublishedArticles.objects.all()
+    context = {'object_list': articles, 'tags': tags}
     return render(request, template, context)
